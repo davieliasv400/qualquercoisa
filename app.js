52592258cfaa754b250f190aa3efe3743889df60
davieliasv400 <blacklitchv400@gmail.com>
2025-10-17 11:36:14 -0300

    qualquer coisa

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
-let numerosecreto = gerarNumeroAleatorio();
+let listadenumerossorteados = [];
+let numerolimite = 10;
+let numerosecreto = gerarNumeroAleatorio()
+let tentativas = 1;
 
 function exibirtextonatela (tag, texto){
-    let chute = document.querySelector('input').value;
-    
-    if (chute == numerosecreto) {
-        exibirtextonatela('h1', ' acertou');
-        let palavratentativa = tentativas > 1 ? 'tentativas' :
-        'tentativa';
-        let mensagemtentativas = 'voce descobriu o numero secreto com'
-    }
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsivevoice.speak(texto, 'brazilian portuguese female', {rate:1.2});
 }
 
-exibirtextonatela('h1', 'jogo do numero secreto');
-exibirtextonatela('p', 'escolha um numero entre 1 e 10');
+function exibirtextonatela() {
+    exibirtextonatela('h1', 'jogo do numero secreto');
+    exibirtextonatela('p', 'escolha um numero entre 1 e 10');
+}
 
 function verificarchute(){
     let chute = documen.querySelector('input').value;
-    console.log(chute == numerosecreto);
+
+    if(chute == numerosecreto) {
+        exibirtextonatela('h1', 'acertou!');
+        let palavratentativa = tentativas > 1 ? 'tentativas' : 'tentativas';
+        let mensagemtentativas = `voce descobriu o numero secreto com ${tentativas} ${palavratentativa}!`;
+        exibirtextonatela('p', mensagemtentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numerosecreto) {
+            exibirtextonatela('p', 'o numero secreto e menor');
+        } else {
+            exibirtextonatela('p', 'o numero secreto e maior');
+        }
+        tentativas++;
+        
+    }
 }
+
+
+
+
+
 
 function gerarNumeroAleatorio(){
     return perseint(Math.random() * 10 + 1);
